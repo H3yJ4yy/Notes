96c9b35d6c21e288391a473e7c5f28b15fbcf5fd
H3yJ4yy <143395836+H3yJ4yy@users.noreply.github.com>
2023-11-28 16:30:40 -0600

    worked on apihtml

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const fileName = './db/db.json'
 
 router.get('/notes', (req, res) => {
@@ -10,7 +12,16 @@ router.get('/notes', (req, res) => {
 } )
 
 router.post("/notes", (req, res) => {
-    res.json('success');
+    const { title, text} = req.body;
+    const myNote = {
+        title, 
+        text, 
+        id: uuidv4()
+    }
+    const newNote = [...fileName, myNote];
+    const data = fs.writeFileSync('db/db.json', JSON.stringify(newNote), (err) => console.log(err));
+    res.json(data);
+
 })
 
 module.exports = router;

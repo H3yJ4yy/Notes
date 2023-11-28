c6979542ee8bda0482cc89b28743b07393c1a1fb
H3yJ4yy <143395836+H3yJ4yy@users.noreply.github.com>
2023-11-28 16:17:25 -0600

    updates to html routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const routes = require('./routes')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+//middleware to parse json 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static files from public directory
+app.use(express.static('public'));
+
+//defined routes used
+app.use(routes);
+
+// starts server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
